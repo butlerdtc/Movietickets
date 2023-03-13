f412774bd1f423e5c54af479103d6330bccae200
butlerdtc <butlerr2@middleton.school.nz>
2023-03-14 11:09:58 +1300

    Update movie_tickets_v1.py

--- movie_tickets_v1.py
+++ movie_tickets_v1.py
@@ -2,3 +2,20 @@
 Welcome screen and set up variables
 Created by Robson Butler
 """
+
+# Component 1 - Welcome screen and set up variables
+
+
+def sell_ticket():
+    print("****** Fun movies - ticketing system ******\n")
+
+    adult_tickets = 0
+    child_tickets = 0
+    student_tickets = 0
+    gift_vouchers = 0
+    total_sales = 0
+    tickets_sold = 0
+
+
+# Main routine
+sell_ticket()
